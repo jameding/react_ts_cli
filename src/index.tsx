ecf27254dd3174dab496e0d91c9a435dc8e6f481
jameding <dingjiemu@jinghang.ai>
2020-09-18 15:19:16 +0800

    初始文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { Provider } from 'react-redux';
+import './style/main.less';
+// import App from './views/App';
 import * as serviceWorker from './serviceWorker';
+import RouterConfig from './router/index';
+import store from './redux/store/index';
+// console.log(process.env);
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+	<React.StrictMode>
+		<Provider store={store}>
+			<RouterConfig />
+		</Provider>
+	</React.StrictMode>,
+	// <React.StrictMode>
+	// 	<App />
+	// </React.StrictMode>,
+	document.getElementById('root')
 );
 
 // If you want your app to work offline and load faster, you can change
